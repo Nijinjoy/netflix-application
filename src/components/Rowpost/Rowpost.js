ec098c71ac15a9bb150ef760a45dce67c511d0a6
nijinjoy <nijinjoy1999@gmail.com>
2023-04-26 16:02:04 +0530

    card content integration

--- src/components/Rowpost/Rowpost.js
+++ src/components/Rowpost/Rowpost.js
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Rowpost.css'
+import axios from '../../axios'
+import { api_key,baseUrl, imageUrl } from '../../constants/constants'
 
 function Rowpost() {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    axios.get(`discover/tv?api_key=${api_key}&with_networks=213`).then(response => {
+      console.log(response.data);
+      setMovies(response.data.results)
+    }).catch(err => {
+      // alert('Network error')
+    })
+  }, [])
   return (
     <div className='row'>
-        <h2>Title</h2>
-        <div className='posters'>
-        <img className='poster' src="https://dailyresearchplot.com/wp-content/uploads/2021/09/Money-Heist-Season-5-Volume-1.jpg" alt="poster" />
-        <img className='poster' src="https://dailyresearchplot.com/wp-content/uploads/2021/09/Money-Heist-Season-5-Volume-1.jpg" alt="poster" />
-        <img className='poster' src="https://dailyresearchplot.com/wp-content/uploads/2021/09/Money-Heist-Season-5-Volume-1.jpg" alt="poster" />
-        <img className='poster' src="https://dailyresearchplot.com/wp-content/uploads/2021/09/Money-Heist-Season-5-Volume-1.jpg" alt="poster" />
-        <img className='poster' src="https://dailyresearchplot.com/wp-content/uploads/2021/09/Money-Heist-Season-5-Volume-1.jpg" alt="poster" />
-        <img className='poster' src="https://dailyresearchplot.com/wp-content/uploads/2021/09/Money-Heist-Season-5-Volume-1.jpg" alt="poster" />
-        </div>
-       
+      <h2>Netflix Originals</h2>
+      <div className='posters'>
+        {
+          movies.map((obj) => 
+            <img className='poster' src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
+          )  }
+      </div>
+
     </div>
   )
 }
